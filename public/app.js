31e9306bdd90a63734aa48aacb420aff2bbbd91c
owonikoko oluwaseun <oluwaseun.owonikoko@andela.com>
2017-03-02 13:58:26 +0100

    changes made to the public/app.js file to accomodate the stylesheet.css file

--- public/app.js
+++ public/app.js
@@ -3,15 +3,16 @@ const path = require('path');
 const app = express();
 
 app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public/stylesheet.css'));
 
 app.get('/', function (req, res) {
   console.log(req.url);
-  res.sendFile(path.join(__dirname + '/public/index.html'));
+  res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 app.get('/functions', function (req, res) {
   console.log(req.url);
-  res.sendFile('/functions.html');
+  res.sendFile(path.join(__dirname + '/functions.html'));
 });
 
 app.listen(3000, function () {
