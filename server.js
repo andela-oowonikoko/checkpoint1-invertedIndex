dc2766bfb70b363e699e003e99a094b380010f72
andela-oowonikoko <oluwaseun.owonikoko@andela.com>
2017-03-09 17:35:01 +0100

    added the server.js file

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const app = express();
+
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+const port = process.env.PORT || 8080;
+
+// make express look in the public directory for assets (css/js/img)
+app.use(express.static(`${__dirname}/public`));
+
+// set the home page route
+app.get('/', (req, res) => {
+  // ejs render automatically looks in the views folder
+  res.render('index');
+});
+
+app.listen(port, () => {
+  console.log(`App started on http://localhost:${port}`);
+});
